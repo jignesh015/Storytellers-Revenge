e0d4ac62f43df8c768275066db64441c53a7df42
Jignesh Patel <patel.jignesh705@gmail.com>
2018-12-20 01:12:34 +0530

    Added basic UI functionality

---
+++ Storytellers Revenge/Assets/Script/MainLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class MainLogic : MonoBehaviour {
+
+	public GameObject videoSphere;
+	public GameObject dummySphere;
+
+	public GameObject startUI;
+
+	public List<string> Urls;
+
+	private VideoPlayer videoPlayer;
+
+	// Use this for initialization
+	void Start () {
+		videoPlayer = videoSphere.transform.GetComponent<VideoPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayVideo() {
+		Debug.Log ("clicked");
+		dummySphere.SetActive (false);
+		videoSphere.SetActive (true);
+		startUI.SetActive (false);
+		InitializeVideoPlayer ();
+	}
+
+	private void InitializeVideoPlayer() {
+		videoPlayer.source = VideoSource.Url;
+		videoPlayer.url = Urls [0];
+		Debug.Log ("Success");
+		videoPlayer.Play ();
+	}
+}
